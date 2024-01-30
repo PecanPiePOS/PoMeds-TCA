e617583898f66efaf641f659afc3b8c2cb36757b
PecanPie <hidra0321@naver.com>
2024-01-30 13:38:35 +0900

    #3 - Repository Implementation 을 구현했습니다

--- Pomeds/Pomeds/Domain/Repository/OngoingMedicationRepository.swift
+++ Pomeds/Pomeds/Domain/Repository/OngoingMedicationRepository.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+import SwiftData
+
+struct OngoingMedicationRepository<DataSource: LocalDataSource>: Repository
+where
+DataSource.Response == MedicationRecordItem,
+DataSource.Request == Bool {
+    
+    typealias Request = Bool
+    typealias Response = [MedicationRecordItem]
+    
+    private let localDataSource: DataSource
+    
+    init(localDataSource: DataSource) {
+        self.localDataSource = localDataSource
+    }
+    
+    func execute(request: Bool?) async throws -> [MedicationRecordItem] {
+        return try await localDataSource.list(request: true)
+    }
+}
