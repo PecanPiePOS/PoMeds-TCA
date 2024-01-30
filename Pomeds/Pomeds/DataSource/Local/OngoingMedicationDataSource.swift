2b0ee5579a0c8e9927c9c85e8ecc97201f72cbd3
PecanPie <hidra0321@naver.com>
2024-01-30 13:39:18 +0900

    #3 - 현재 복용중인 리스트를 불러오는 데이터소스의 Impl 을 구현했습니다

--- Pomeds/Pomeds/DataSource/Local/OngoingMedicationDataSource.swift
+++ Pomeds/Pomeds/DataSource/Local/OngoingMedicationDataSource.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+
+import RealmSwift
+
+struct OngoingMedicationDataSource: LocalDataSource {
+    
+    typealias Request = Bool
+    typealias Response = MedicationRecordItem
+
+    private let realm = try! Realm()
+    
+    private init() {}
+    
+    func list(request: Bool) async throws -> [MedicationRecordItem] {
+        var list: [Response] = []
+        realm.objects(Response.self)
+            .filter {$0.isTakingNow == request}
+            .forEach {
+            list.append($0)
+        }
+        return list
+    }
+    
+    func add(entity: MedicationRecordItem) async throws -> Bool {
+        do {
+            try realm.write {
+                realm.add(entity)
+            }
+            return true
+        } catch let error {
+            print("Error adding data: <\(error)>")
+            return false
+        }
+    }
+    
+    func delete(id: ObjectId) async throws -> Bool {
+        let deletingItem = realm.objects(Response.self).filter {$0._id == id}
+        do {
+            try realm.write {
+                realm.delete(deletingItem)
+            }
+            return true
+        } catch let error {
+            print("Error deleting data: <\(error)>")
+            return false
+        }
+    }
+}
