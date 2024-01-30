3fb8611b86033fe4ccc8d269260cdff30d6ac212
PecanPie <hidra0321@naver.com>
2024-01-30 13:37:53 +0900

    #3 - DataSource 의 Interface 를 구현했습니다

--- Pomeds/Pomeds/DataSource/Local/LocalDataSource.swift
+++ Pomeds/Pomeds/DataSource/Local/LocalDataSource.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+import RealmSwift
+
+protocol LocalDataSource {
+    associatedtype Request
+    associatedtype Response
+    
+    func list(request: Bool) async throws -> [Response]
+    func add(entity: Response) async throws -> Bool
+    func delete(id: ObjectId) async throws -> Bool
+}
