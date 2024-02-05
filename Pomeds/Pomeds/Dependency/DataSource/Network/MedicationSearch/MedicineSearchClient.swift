f6c7d4130b62cdfae086ca2698845c49ff08455b
PecanPie <hidra0321@naver.com>
2024-02-06 00:03:04 +0900

    Using Open API client is added

--- Pomeds/Pomeds/Dependency/DataSource/Network/MedicationSearch/MedicineSearchClient.swift
+++ Pomeds/Pomeds/Dependency/DataSource/Network/MedicationSearch/MedicineSearchClient.swift
@@ -5,15 +5,27 @@
 //  Created by KYUBO A. SHIM on 1/31/24.
 //
 
+import Combine
 import Foundation
 
 import ComposableArchitecture
 
 @DependencyClient
 struct MedicineSearchClient {
-    var fetch: (_ medicineName: String) async throws -> [MedicationResponse]
+    var fetch: (_ medicineName: String) async throws -> AnyPublisher<MedicationResponse, APIError>
 }
 
-extension MedicineSearchClient {
-    
+extension MedicineSearchClient: DependencyKey {
+    static let liveValue = Self (
+        fetch: { medicineName in
+            return try await MedicineSearchService.shared.getMedicine(requset: medicineName)
+        }
+    )
+}
+
+extension DependencyValues {
+    var medicineSearchData: MedicineSearchClient {
+        get { self[MedicineSearchClient.self] }
+        set { self[MedicineSearchClient.self] = newValue }
+    }
 }
