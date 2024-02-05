d7f1aee168e441ed41c35cb575ba53886c8f7993
PecanPie <hidra0321@naver.com>
2024-02-05 23:56:18 +0900

    Added Needed extension touching UIKit

--- Pomeds/Pomeds/Resources/Extensions/Encodable+.swift
+++ Pomeds/Pomeds/Resources/Extensions/Encodable+.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+extension Encodable {
+    
+  func asParameter() throws -> [String: Any] {
+    let data = try JSONEncoder().encode(self)
+    guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+            as? [String: Any] else {
+      throw NSError()
+    }
+    
+    return dictionary
+  }
+}
--- Pomeds/Pomeds/Resources/Extensions/UINavigationController+.swift
+++ Pomeds/Pomeds/Resources/Extensions/UINavigationController+.swift
@@ -5,4 +5,11 @@
 //  Created by KYUBO A. SHIM on 2/2/24.
 //
 
-import Foundation
+import UIKit
+
+extension UINavigationController {
+    // Remove back button text
+//    open override func viewWillLayoutSubviews() {
+//        navigationBar.topItem?.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+//    }
+}
