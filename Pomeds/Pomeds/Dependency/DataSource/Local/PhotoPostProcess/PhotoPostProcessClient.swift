794a47a8b0c9d0967c93e07782aad30ecb96e110
PecanPie <hidra0321@naver.com>
2024-02-06 00:02:02 +0900

    Post Process of the caputred Image related client and helper are added
    Use only the middel of the image

--- Pomeds/Pomeds/Dependency/DataSource/Local/PhotoPostProcess/PhotoPostProcessClient.swift
+++ Pomeds/Pomeds/Dependency/DataSource/Local/PhotoPostProcess/PhotoPostProcessClient.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import UIKit
+import SwiftUI
+
+import ComposableArchitecture
+
+@DependencyClient
+struct PhotoPostProcessClient {
+    var cropData: (_ originalData: Data) async throws -> Data
+}
+
+extension PhotoPostProcessClient: DependencyKey {
+    static let liveValue = Self (
+        cropData: { data in
+            return try await PhotoPostProcessHelper.cropDataInTheMiddle(of: data)
+        }
+    )
+}
+
+extension DependencyValues {
+    var cropDataWhereNeeded: PhotoPostProcessClient {
+        get { self[PhotoPostProcessClient.self] }
+        set { self[PhotoPostProcessClient.self] = newValue }
+    }
+}
