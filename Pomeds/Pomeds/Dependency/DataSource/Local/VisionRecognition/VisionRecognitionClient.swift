222bbea4efd6d693c40e6d647abf30de1dfe72cf
PecanPie <hidra0321@naver.com>
2024-02-06 00:01:11 +0900

    VisionKit related Dependency and Helper are added

--- Pomeds/Pomeds/Dependency/DataSource/Local/VisionRecognition/VisionRecognitionClient.swift
+++ Pomeds/Pomeds/Dependency/DataSource/Local/VisionRecognition/VisionRecognitionClient.swift
@@ -1,8 +1,31 @@
 //
-//  VisionRecognition.swift
+//  VisionRecognitionClient.swift
 //  Pomeds
 //
 //  Created by KYUBO A. SHIM on 1/31/24.
 //
 
+import UIKit
 import Foundation
+
+import ComposableArchitecture
+
+@DependencyClient
+struct VisionRecognitionClient {
+    var accept: (_ photoData: [Data]) async throws -> [String?]
+}
+
+extension VisionRecognitionClient: DependencyKey {
+    static let liveValue = Self (
+        accept: { images in
+            return try await VisionRecognitionHelper.shared.getRecognizedText(with: images)
+        }
+    )
+}
+
+extension DependencyValues {
+    var visionRecognizer: VisionRecognitionClient {
+        get { self[VisionRecognitionClient.self] }
+        set { self[VisionRecognitionClient.self] = newValue }
+    }
+}
--- Pomeds/Pomeds/Dependency/DataSource/Local/VisionRecognition/VisionRecognitionHelper.swift
+++ Pomeds/Pomeds/Dependency/DataSource/Local/VisionRecognition/VisionRecognitionHelper.swift
@@ -5,4 +5,58 @@
 //  Created by KYUBO A. SHIM on 1/31/24.
 //
 
-import Foundation
+import Combine
+import UIKit
+import Vision
+import VisionKit
+
+final class VisionRecognitionHelper: ObservableObject {
+    
+    static let shared = VisionRecognitionHelper()
+    private init() {}
+    
+    private var ocrStrings: [String?] = []
+    private var count: Int = 0
+    
+    func getRecognizedText(with images: [Data]) async throws -> [String?] {
+        defer {
+            print(ocrStrings, "📌📌📌📌📌")
+            ocrStrings = []
+        }
+        for imageData in images {
+            await recognizeText(with: UIImage(data: imageData) ?? UIImage())
+        }
+        return ocrStrings
+    }
+
+    private func recognizeText(with image: UIImage) async {
+        guard let image = image.cgImage else {
+            ocrStrings.append(nil)
+            count += 1
+            return }
+        
+        let handler = VNImageRequestHandler(cgImage: image, options: [:])
+        let request = VNRecognizeTextRequest { [weak self] request, error in
+            guard let result = request.results as? [VNRecognizedTextObservation], error == nil else { return }
+            let text = result.compactMap { $0.topCandidates(1).first?.string }
+                .joined(separator: "\n")
+            
+            self?.ocrStrings.append(text)
+            self?.count += 1
+        }
+        request.revision = VNRecognizeTextRequestRevision3
+        request.recognitionLanguages = ["ko-KR", "en-US"]
+        request.recognitionLevel = .accurate
+        request.usesLanguageCorrection = true
+        
+        do {
+            try handler.perform([request])
+        } catch let error {
+            print(error)
+            ocrStrings.append(nil)
+            self.count += 1
+        }
+    }
+}
+
+// 참조: https://leetaek.tistory.com/76
