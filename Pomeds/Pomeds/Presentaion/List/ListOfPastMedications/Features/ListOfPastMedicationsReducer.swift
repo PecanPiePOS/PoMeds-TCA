40f39847a4c7cac886c06a29516eaa45d858ff25
PecanPie <hidra0321@naver.com>
2024-02-09 15:38:05 +0900

    [Feat] #7 - 과거 복용 리스트에 대한 Feature 를 추가했습니다.

--- Pomeds/Pomeds/Presentaion/List/ListOfPastMedications/Features/ListOfPastMedicationsReducer.swift
+++ Pomeds/Pomeds/Presentaion/List/ListOfPastMedications/Features/ListOfPastMedicationsReducer.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+import ComposableArchitecture
+import RealmSwift
+
+@Reducer
+struct ListOfPastMedicationsReducer {
+    typealias MedicationData = MedicationRecordItem
+    
+    @Dependency(\.medicationDatabase) var database
+    
+    @ObservableState
+    struct State: Equatable {
+        var pastMedicationList: [MedicationData] = []
+        var pastSupplementList: [MedicationData] = []
+    }
+    
+    enum Action {
+        case onAppear
+        case medicationListResponse([MedicationData])
+        case supplementsListResponse([MedicationData])
+        
+        case cellDidTapWith(id: ObjectId, title: String)
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .onAppear:
+                return .run { send in
+                    let pastList = try await self.database.fetchPastMedications()
+                    await send(.medicationListResponse(pastList.filter({ $0.medicationType == "med" })))
+                    await send(.supplementsListResponse(pastList.filter{ $0.medicationType == "suppl" }))
+                }
+            case let .medicationListResponse(list):
+                state.pastMedicationList = list
+                return .none
+            case let .supplementsListResponse(list):
+                state.pastSupplementList = list
+                return .none
+            case let .cellDidTapWith(id, title):
+                return .none
+            }
+        }
+    }
+}
--- Pomeds/Pomeds/Presentaion/List/ListOfPastMedications/Views/ListOfPastMedicationsView.swift
+++ Pomeds/Pomeds/Presentaion/List/ListOfPastMedications/Views/ListOfPastMedicationsView.swift
@@ -7,12 +7,18 @@
 
 import SwiftUI
 
+import ComposableArchitecture
+
 struct ListOfPastMedicationsView: View {
+    @State var store: StoreOf<ListOfPastMedicationsReducer>
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ListOfPastMedicationsView()
+    ListOfPastMedicationsView(store: Store(initialState: ListOfPastMedicationsReducer.State(), reducer: {
+        ListOfPastMedicationsReducer()
+    }))
 }
